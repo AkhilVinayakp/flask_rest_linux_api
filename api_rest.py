daf816c3bfe80d2f85b4a310ddca1cf1dcff3c0d
AkhilVinayakp <nirvanaosborn@gmail.com>
2020-03-30 10:16:57 +0530

    adding subprocess module for testing

--- api_rest.py
+++ api_rest.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask,jso
 from flask_restful import Api, Resource
+import request
+import os
+import subprocess
 app = Flask(__name__)
 api = Api(app)
+
+# adding a class that will resolve according to that
+
+
 class Hello_world(Resource):
     def get(self):
         return {'hello': 'world'}
-api.add_resource(Hello_world,'/')
-if __name__=='__main__':
+
+
+api.add_resource(Hello_world, '/')
+
+
+if __name__ == '__main__':
     app.run(debug='true')
