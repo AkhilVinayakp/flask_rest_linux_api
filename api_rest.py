d083824ce2015ca8edaf626ab4d0eae67a6362c4
AkhilVinayakp <nirvanaosborn@gmail.com>
2020-03-30 13:53:41 +0530

    html return of command execution ok

--- api_rest.py
+++ api_rest.py
@@ -1,7 +1,7 @@
-from flask import Flask,jso
+from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
-import request
 import os
+import shlex
 import subprocess
 app = Flask(__name__)
 api = Api(app)
@@ -14,7 +14,18 @@ class Hello_world(Resource):
         return {'hello': 'world'}
 
 
+@app.route('/linapi', methods=['GET'])
+def resolve():
+    cmd = request.args.get('cmd')
+    cmd = shlex.split(cmd)
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = proc.communicate()
+    stdout = str(stdout).replace('\\n', '<br>')
+    return "command <br>{} output <br> {} <br>error<br> {} ".format(cmd,stdout,stderr)
+
+
 api.add_resource(Hello_world, '/')
+#api.add_resource(Api_call, '/lin_api')
 
 
 if __name__ == '__main__':
