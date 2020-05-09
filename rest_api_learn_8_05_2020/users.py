44ae489de565a2016a8d1450521b5f51422362f9
AkhilVinayakp <nirvanaosborn@gmail.com>
2020-05-09 12:45:09 +0530

    adding fetch_by_name functionality to Users class database connection added

--- rest_api_learn_8_05_2020/users.py
+++ rest_api_learn_8_05_2020/users.py
@@ -1,7 +1,23 @@
+# getting ready to access the database test.db
+import sqlite3
+
+
 class Users:
     def __init__(self, _id, name, password):
         self.id = _id
         self.name = name
         self.password = password
+
+    # method to retrieve the user when we know the user name
+    @classmethod
+    def find_by_name(cls, name):
+        connection = sqlite3.connect('test.db')
+        cursor = connection.cursor()
+        row = cursor.execute('select * from test where name=?', (name,)).fetchone()
+        connection.close()
+        if row:
+            return cls(*row)
+        else:
+            return None
 
 
